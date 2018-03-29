ea2f65356ff1d3d42e7570a8fa4b897d3db8b5d2
n0an <nantog@gmail.com>
2018-03-29 15:41:26 +0300

    new extensions added

---
+++ _!EXTENSIONS/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  SensEye
+//
+//  Created by Anton Novoselov on 23/03/2018.
+//  Copyright © 2018 Anton Novoselov. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let ANNotificationAuthorizationCompleted = Notification.Name("NotificationAuthorizationCompleted")
+}
---
+++ _!EXTENSIONS/UITextField+Placeholder.swift
@@ -0,0 +1,23 @@
+//
+//  UITextField+Placeholder.swift
+//  SensEye
+//
+//  Created by Anton Novoselov on 16/01/2017.
+//  Copyright © 2017 Anton Novoselov. All rights reserved.
+//
+
+import UIKit
+
+extension UITextField {
+    @IBInspectable var placeHolderColor: UIColor? {
+        get {
+            return self.placeHolderColor
+        }
+        
+        set {
+            if let placeholder = self.placeholder {
+                self.attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [NSForegroundColorAttributeName: newValue!])
+            }
+        }
+    }
+}
